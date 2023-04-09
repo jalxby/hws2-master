bde6fdda023bc647ec6a5a3758b78f9215d136c4
jalxby <jalxby@gmail.com>
2023-04-09 20:37:08 +0200

    hw11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -17,6 +17,13 @@ function HW11() {
 
     const change = (event: any, value: any) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+    if(Array.isArray(value)){
+        setValue1(value[0])
+        setValue2(value[1])
+    }else {
+        setValue1(value)
+    }
+
     }
 
     return (
@@ -29,6 +36,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
+                            onChange={change}
+                            value={value1}
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -36,6 +45,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
+                            onChange={change}
+                            value={[value1,value2]}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
